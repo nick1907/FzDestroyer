f66eb25dcaae1cef69a69a61b55670fc837a4390
brzydki <luxintenebris21@gmail.com>
2015-05-26 09:13:16 +0200

    Wstawiam najnowsza wersje projektu

--- app/src/main/java/destroyer/friendzone/com/fzdestroyer/Ustawienia.java
+++ app/src/main/java/destroyer/friendzone/com/fzdestroyer/Ustawienia.java
@@ -2,12 +2,23 @@ package destroyer.friendzone.com.fzdestroyer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -22,6 +33,7 @@ public class Ustawienia extends ActionBarActivity
     Spinner wybor_miesiaca;
     Spinner wybor_roku;
 
+    String wybrana_orientacja;
     EditText opis_pole; // opis uzytkownika (o sobie)
     EditText zasieg_pole; // zasieg w kilometrach potrzebny do poszukiwan drugiej "polowki"
     EditText miejscowosc_pole; // pole na miejscowosc
@@ -30,6 +42,10 @@ public class Ustawienia extends ActionBarActivity
     ArrayList<String> dni;
     ArrayList<String> miesiace;
     ArrayList<String> lata;
+
+    String wybrany_dzien;
+    String wybrany_miesiac;
+    String wybrany_rok;
 
     Bundle dane;
 
@@ -40,6 +56,8 @@ public class Ustawienia extends ActionBarActivity
         setContentView(R.layout.activity_ustawienia);
 
         dane = getIntent().getExtras();
+        int profil_ID = dane.getInt("login", 0);
+        String pass = dane.getString("haslo");
 
         // przypisanie odpowiednich pol tekstowych
         opis_pole = (EditText) findViewById(R.id.pole_opisu);
@@ -53,10 +71,25 @@ public class Ustawienia extends ActionBarActivity
         orientacja.add("biseksualna");
         orientacja.add("homoseksualna");
 
+        wybrana_orientacja = "";
+
         // dodanie adaptera do spinnera
         ArrayAdapter<String> orientacja_adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, orientacja);
         orientacja_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         wybor_orientacji.setAdapter(orientacja_adapter);
+        wybor_orientacji.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                wybrana_orientacja = orientacja.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
 
         // ustawienie listy z dniami miesiecy
         dni = new ArrayList<>();
@@ -68,6 +101,23 @@ public class Ustawienia extends ActionBarActivity
         ArrayAdapter adapter_dni = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dni);
         adapter_dni.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         wybor_dnia.setAdapter(adapter_dni);
+
+        wybrany_dzien = "";
+
+        wybor_dnia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                wybrany_dzien = dni.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });
 
         // wstawine do listy nazw miesiecy
         miesiace = new ArrayList<>();
@@ -84,7 +134,7 @@ public class Ustawienia extends ActionBarActivity
         miesiace.add("Listopad");
         miesiace.add("Grudzień");
 
-        // TODO
+        // w przyszlosci zaimplementowac:
         // Spinner.setSelection(int position, boolean animate)
 
         // dodanie adaptera do spinnera z miesiacami
@@ -92,6 +142,21 @@ public class Ustawienia extends ActionBarActivity
         ArrayAdapter adapter_miesiecy = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, miesiace);
         adapter_miesiecy.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         wybor_miesiaca.setAdapter(adapter_miesiecy);
+
+        wybrany_miesiac = "";
+        wybor_miesiaca.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                wybrany_miesiac = Integer.toString(position + 1);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
 
         // pobranie aktualnego roku
         Calendar kalendarz = Calendar.getInstance();
@@ -103,11 +168,26 @@ public class Ustawienia extends ActionBarActivity
         for (int i = rok - 10; i > (rok - 66) ; --i)
             lata.add(Integer.toString(i));
 
+        wybrany_rok = "";
+
         // dodanie adaptera do spinnera z latami
         wybor_roku = (Spinner) findViewById(R.id.wybor_roku);
         ArrayAdapter<String> adapter_lat = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, lata);
         adapter_lat.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         wybor_roku.setAdapter(adapter_lat);
+        wybor_roku.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                wybrany_rok = lata.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
     }
 
     @Override
@@ -138,6 +218,71 @@ public class Ustawienia extends ActionBarActivity
     // metoda do aktualizacji danych w bazie danych
     public void zaaktualizujDane(View widok)
     {
+        String opis = opis_pole.getText().toString();
+        int zasieg = 0;
+        if (!zasieg_pole.getText().toString().isEmpty())
+            zasieg = Integer.parseInt(zasieg_pole.getText().toString(), 0);
+        String miejscowosc = miejscowosc_pole.getText().toString();
 
+        int profil_ID = dane.getInt("login", 0);
+        String pass = dane.getString("haslo");
+
+        // utworzenie zapytania do bazy danych i jego wykoanie
+        try
+        {
+            int rozmiar = 0;
+
+            // zapytanie do bazy danych (zmiana danych uzytkownika programu)
+            String data = URLEncoder.encode("login", "UTF-8") + "=" + URLEncoder.encode(Integer.toString(profil_ID), "UTF-8");
+            data += "&" + URLEncoder.encode("haslo", "UTF-8") + "=" + URLEncoder.encode("supertajne", "UTF-8");
+            rozmiar = data.length();
+            if (!opis.isEmpty()) // jesli opis nie jest pusty
+                data += "&" + URLEncoder.encode("opis", "UTF-8") + "=" + URLEncoder.encode(opis, "UTF-8");
+            if (zasieg > 0) // jesli podano zasieg
+                data += "&" + URLEncoder.encode("zasieg", "UTF-8") + "=" + URLEncoder.encode(Integer.toString(zasieg), "UTF-8");
+            if (!miejscowosc.isEmpty()) // jesli podano miejscowosc
+                data += "&" + URLEncoder.encode("miejscowosc", "UTF-8") + "=" + URLEncoder.encode(miejscowosc, "UTF-8");
+            if (!wybrana_orientacja.isEmpty()) // jesli wybrano orientacje
+                data += "&" + URLEncoder.encode("orientacja", "UTF-8") + "=" + URLEncoder.encode(wybrana_orientacja, "UTF-8");
+            if (!wybrany_dzien.isEmpty()) // jesli wybrano dzien miesiaca
+                data += "&" + URLEncoder.encode("dzien_urodzenia", "UTF-8") + "=" + URLEncoder.encode(wybrany_dzien, "UTF-8");
+            if (!wybrany_miesiac.isEmpty()) // jesli wybrano miesiac
+                data += "&" + URLEncoder.encode("miesiac_urodzenia", "UTF-8") + "=" + URLEncoder.encode(wybrany_miesiac, "UTF-8");
+            if (!wybrany_rok.isEmpty()) // jesli wybrano rok
+                data += "&" + URLEncoder.encode("rok_urodzenia", "UTF-8") + "=" + URLEncoder.encode(wybrany_rok, "UTF-8");
+
+            try
+            {
+                URL url = new URL("http://vigorous-cheetah-65-226242.euw1.nitrousbox.com/update_uzytkownika.php");
+
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                // wyslij zapytanie do bazy danych
+                // String text = "";
+                BufferedReader reader;
+                wr.write(data);
+                wr.flush();
+
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                // odczytaj odpowiedz serwera
+                while ((line = reader.readLine()) != null)
+                    sb.append(new StringBuilder(line + "\n"));
+
+                Log.d("wiadomosc", sb.toString());
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        } catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+        finish();
     }
 }
