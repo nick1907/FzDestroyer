7e0c00bf56924dcbc8ee1c44fdb0fbe38b52559e
brzydki <luxintenebris21@gmail.com>
2015-05-26 14:00:10 +0200

    To jest kod który już powinien sporo móc zaoferować

--- app/src/main/java/destroyer/friendzone/com/fzdestroyer/AdapterWiadomosci.java
+++ app/src/main/java/destroyer/friendzone/com/fzdestroyer/AdapterWiadomosci.java
@@ -9,8 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class AdapterWiadomosci extends ArrayAdapter<Wiadomosc>
 {
     Context kontekst;
