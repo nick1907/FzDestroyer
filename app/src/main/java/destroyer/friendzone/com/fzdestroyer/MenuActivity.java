8ea582132a32392eddf3ed0e8279ca7d6b66c319
brzydki <luxintenebris21@gmail.com>
2015-05-30 10:14:23 +0200

    Zczytanie projektu

--- app/src/main/java/destroyer/friendzone/com/fzdestroyer/MenuActivity.java
+++ app/src/main/java/destroyer/friendzone/com/fzdestroyer/MenuActivity.java
@@ -2,6 +2,7 @@ package destroyer.friendzone.com.fzdestroyer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -11,6 +12,8 @@ public class MenuActivity extends Activity
 {
     ImageButton przycisk_czatu;
     ImageButton przycisk_ustawien;
+    ImageButton przycisk_pomocy;
+    ImageButton przycisk_info;
     Intent intent;
     Activity aktywnosc = this;
 
@@ -21,12 +24,15 @@ public class MenuActivity extends Activity
         setContentView(R.layout.activity_menu);
 
         przycisk_czatu = (ImageButton) findViewById(R.id.przycisk_czatu);
-        przycisk_czatu.setOnClickListener(new View.OnClickListener()
-        {
+        przycisk_czatu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 intent = new Intent(aktywnosc, Czat.class);
+                SharedPreferences settings = getSharedPreferences("PREF", 0);
+                if (settings.getString("profil", "").equals("882998035056017"))
+                    intent.putExtra("profil_odbiorcy", "900358920032103");
+                else
+                    intent.putExtra("profil_odbiorcy", "882998035056017");
                 startActivity(intent);
             }
         });
@@ -41,5 +47,25 @@ public class MenuActivity extends Activity
                 startActivity(intent);
             }
         });
+        przycisk_pomocy = (ImageButton) findViewById(R.id.przycisk_pomocy);
+        przycisk_pomocy.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(aktywnosc, Pomoc.class);
+                startActivity(intent);
+            }
+        });
+
+        przycisk_info = (ImageButton) findViewById(R.id.przycisk_info);
+        przycisk_info.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(aktywnosc, Infos.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
